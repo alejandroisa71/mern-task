64ca030c86ce6bde426faaa6ea0e92fc35db8430
Alejandro Isa <alejandroisa@gmail.com>
2021-04-13 01:16:35 -0300

    Se agrega State y funciones para el Componente Login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const onChange = () => {};
+  //State para iniciar sesion
+  const [usuario, guardarUsuario] = useState({
+    email: "",
+    password: "",
+  });
+
+  //extraer de usuario
+  const { email, password } = usuario;
+
+  const onChange = (e) => {
+    guardarUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //Cuando el usuario quiere inicar sesion
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    //Validar que no haya campos vacios
+
+    //Pasarlo al action
+  };
+
   return (
     <div className="form-usuario">
       <div className="contenedor-form sombra-dark">
         <h1>iniciar Sesión</h1>
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="campo-form">
             <label htmlFor="email">Email</label>
             <input
@@ -14,6 +39,7 @@ const Login = () => {
               id="email"
               name="email"
               placeholder="Tu Email"
+              value={email}
               onChange={onChange}
             />
           </div>
@@ -24,6 +50,7 @@ const Login = () => {
               id="password"
               name="password"
               placeholder="Tu Password"
+              value={password}
               onChange={onChange}
             />
           </div>
@@ -35,6 +62,9 @@ const Login = () => {
             />
           </div>
         </form>
+        <Link to={"/nueva-cuenta"} className="enlace-cuenta">
+          Obtener Cuenta
+        </Link>
       </div>
     </div>
   );
