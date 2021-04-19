05ed39bee7689215c3af92be59d55416d4da356b
Alejandro Isa <alejandroisa@gmail.com>
2021-04-18 23:21:28 -0300

    Se llama a los proyectos con disptach

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -2,16 +2,18 @@ import React, { useReducer } from "react";
 
 import proyectoContext from "./proyectoContext";
 import proyectoReducer from "./proyectoReducer";
-import { FORMULARIO_PROYECTO } from "../../types";
+import { FORMULARIO_PROYECTO, OBTENER_PROYECTOS } from "../../types";
 
 const ProyectoState = (props) => {
+  const proyectos = [
+    { id: 1, nombre: "Tienda Virtual" },
+    { id: 2, nombre: "Intranet" },
+    { id: 3, nombre: "Diseño de sitio web" },
+    { id: 4, nombre: "MERN" },
+  ];
+
   const initialState = {
-    proyectos: [
-      { id: 1, nombre: "Tienda Virtual" },
-      { id: 2, nombre: "Intranet" },
-      { id: 3, nombre: "Diseño de sitio web" },
-      { id: 4, nombre: "MERN" },
-    ],
+    proyectos: [],
     formulario: false,
   };
 
@@ -26,12 +28,21 @@ const ProyectoState = (props) => {
     });
   };
 
+  //Obtener los proyectos
+  const obtenerProyectos = () => {
+    dispatch({
+      type: OBTENER_PROYECTOS,
+      payload: proyectos,
+    });
+  };
+
   return (
     <proyectoContext.Provider
       value={{
         proyectos: state.proyectos,
         formulario: state.formulario,
         mostrarFormulario,
+        obtenerProyectos,
       }}
     >
       {props.children}
