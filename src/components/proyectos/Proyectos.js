e075a0e19bbc887b39fa8a0c07204a9c3b5e2fcc
Alejandro Isa <alejandroisa@gmail.com>
2021-04-19 01:14:07 -0300

    Se agrega componente Barra

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,21 +1,20 @@
 import React from "react";
+import Barra from "../layouts/Barra";
 import Sidebar from "../layouts/Sidebar";
 
 const Proyectos = () => {
-  return(
-  <div className="contenedor-app">
-      <Sidebar/>
+  return (
+    <div className="contenedor-app">
+      <Sidebar />
 
-    <div className="seccion-principal">
-      <main>
-
-        <div className="contenedor-tareas">
-
-        </div>
-      </main>
+      <div className="seccion-principal">
+        <Barra />
+        <main>
+          <div className="contenedor-tareas"></div>
+        </main>
+      </div>
     </div>
-  </div>
-  )
+  );
 };
 
 export default Proyectos;
