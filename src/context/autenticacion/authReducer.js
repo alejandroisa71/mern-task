0176e9c78126d836e27362b9dfbcb005b306527e
Alejandro Isa <alejandroisa@gmail.com>
2021-05-01 23:51:39 -0300

    Autenticando al usuario

--- src/context/autenticacion/authReducer.js
+++ src/context/autenticacion/authReducer.js
@@ -9,6 +9,7 @@ import {
 export default (state, action) => {
   switch (action.type) {
     case REGISTRO_EXITOSO:
+    case LOGIN_EXITOSO:
       localStorage.setItem("token", action.payload.token);
       return {
         ...state,
