296d192f79fb172ec1d99f0d6c91194f08750bac
Alejandro Isa <alejandroisa@gmail.com>
2021-05-01 23:22:08 -0300

    Se valida Inicio de Sesion

--- src/context/autenticacion/authState.js
+++ src/context/autenticacion/authState.js
@@ -70,6 +70,24 @@ const AuthState = (props) => {
     }
   };
 
+  //Cuando el usuario inicia sesion
+  const iniciarSesion = async (datos) => {
+    try {
+      const respuesta = await clienteAxios.post("/api/auth", datos);
+      console.log(respuesta);
+    } catch (error) {
+      console.log(error.response.data.msg);
+      const alerta = {
+        msg: error.response.data.msg,
+        categoria: "alerta-error",
+      };
+      dispatch({
+        type: LOGIN_ERROR,
+        payload: alerta,
+      });
+    }
+  };
+
   return (
     <AuthContext.Provider
       value={{
@@ -78,6 +96,7 @@ const AuthState = (props) => {
         usuario: state.usuario,
         mensaje: state.mensaje,
         registrarUsuario,
+        iniciarSesion,
       }}
     >
       {props.children}
