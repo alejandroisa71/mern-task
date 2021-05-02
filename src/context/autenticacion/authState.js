9623c0674fb67dde7d3892ade0cea2c9397c245c
Alejandro Isa <alejandroisa@gmail.com>
2021-05-01 23:02:10 -0300

    Enviando el Token por headers

--- src/context/autenticacion/authState.js
+++ src/context/autenticacion/authState.js
@@ -3,6 +3,7 @@ import AuthContext from "./authContext";
 import AuthReducer from "./authReducer";
 
 import clienteAxios from "../../config/axios";
+import tokenAuth from "../../config/token";
 
 import {
   REGISTRO_EXITOSO,
@@ -32,6 +33,9 @@ const AuthState = (props) => {
         type: REGISTRO_EXITOSO,
         payload: respuesta.data,
       });
+
+      //Obtener el usuario
+      usuarioAutenticado();
     } catch (error) {
       //console.log(error.response.data.msg);
       const alerta = {
@@ -49,11 +53,17 @@ const AuthState = (props) => {
   const usuarioAutenticado = async () => {
     const token = localStorage.getItem("token");
     if (token) {
-      //TODO: para enviar el token por headers
+      tokenAuth(token);
     }
     try {
       const respuesta = await clienteAxios.get("/api/auth");
+      //console.log(respuesta);
+      dispatch({
+        type: OBTENER_USUARIO,
+        payload: respuesta.data.usuario,
+      });
     } catch (error) {
+      console.log(error.response);
       dispatch({
         type: LOGIN_ERROR,
       });
